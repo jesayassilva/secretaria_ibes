0649748b3eb6eda62577a00cc5fbccc1d0841426
egressapp <egress.app@gmail.com>
2018-07-18 03:00:42 -0300

    init

--- secretaria/settings.py
+++ secretaria/settings.py
@@ -26,7 +26,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 ADMINS = [('Jesaias Silva', 'jesayassilva@gmail.com')]
-ALLOWED_HOSTS = ['ibes.herokuapp.com']
+ALLOWED_HOSTS = ['ibes.ml','ibes.herokuapp.com']
 
 
 
